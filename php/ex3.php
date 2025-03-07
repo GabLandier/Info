3fd98e056affa9947c12d9851dafc69f39e1195a
romain <167988710+xyotha@users.noreply.github.com>
2025-03-07 11:43:43 +0100

    Add files via upload

---
+++ php/ex3.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+  <form>
+	<label for="nom">Quel est votre nom ?</label>
+	<input type="text" id="name" name="name"> <br>
+	<label for="prenom">Quel est votre prénom ?</label>
+	<input type="text" id="prenom" name="prenom"><br>
+	<label for=date>Quel est votre date de naissance ?</label>
+	<input type="date" id="date" name="date">
+  
+  </form>
+
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*wampserveur.com*/
